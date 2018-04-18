93a43dc5c2411ac7f57781e94378779168c2f2a1
iosmanthus <20162180053@m.scnu.edu.cn>
2018-04-18 11:41:20 +0800

    Chapter 3

--- chapter3/kernel/kmain.c
+++ chapter3/kernel/kmain.c
@@ -1,18 +1,15 @@
 #include <ktypes.h>
 #include <kvga.h>
-typedef struct _KMultiBoot
-{
-    ;
+typedef struct _KMultiBoot {
+  ;
 } KMultiBoot; // 暂时不做详细定义
 
-void hlt()
-{
-    while(1)
-	__asm__ volatile ("hlt");
+void hlt() {
+  while (1)
+    __asm__ volatile("hlt");
 }
 
-void kmain(KMultiBoot *mb)
-{
-    kvga_puts("hello world");
-    hlt();
+void kmain(KMultiBoot *mb) {
+  kvga_puts("hello world\n");
+  hlt();
 }
